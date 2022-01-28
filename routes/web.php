3f58fea81d8eb5ee2fd4664e437a6868f6499b6a
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2022-01-29 00:18:53 +0700

    configure laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('under-construction');
+if (env('USER_CONSTRUCTION')) {
+    Route::get('/', function () {
+        return view('under-construction');
+    });
+}
+
+Route::group(['prefix' => '/'], function () {
+    //
 });
