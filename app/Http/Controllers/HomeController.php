df0baf2923620d929e27c8843fb80abedd502928
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2022-02-03 12:09:36 +0700

    ~

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
             return view('redirect.maintenance');
         }
 
-        return view('home');
+        // return view('home');
     }
 
     /**
